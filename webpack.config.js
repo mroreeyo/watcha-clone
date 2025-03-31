603a84394431219b2f270168a20613b2996959a2
mroreeyo <oreeyo01@gmail.com>
2025-03-31 18:11:53 +0900

    chore: 프로젝트 설정 파일 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: 'development',
@@ -23,13 +24,28 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    new Dotenv({
+      path: './.env',
+    }),
   ],
   devServer: {
     static: path.resolve(__dirname, 'public'),
