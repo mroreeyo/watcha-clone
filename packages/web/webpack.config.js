448fc2140299e9282b8e663a826b4690dda07e5d
mroreeyo <oreeyo01@gmail.com>
2025-04-07 11:32:50 +0900

    refactor: 모노레포 구조 개선

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
       template: './public/index.html'
     }),
     new Dotenv({
-      path: './.env',
+      path: path.resolve(__dirname, '../../.env'),
     }),
   ],
   devServer: {
