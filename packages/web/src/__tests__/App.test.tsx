392c88181ed3b20c00f90c0faa73d2a822a6cd56
mroreeyo <oreeyo01@gmail.com>
2025-04-09 16:12:51 +0900

    refactor: 프로젝트 구조를 apps와 packages로 분리
    - web과 app을 apps 디렉토리로 이동
    
    - shared는 packages 디렉토리에 유지
    
    - 워크스페이스 설정 업데이트
    
    - webpack alias 경로 수정

--- packages/web/src/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-describe('App 컴포넌트', () => {
-  it('렌더링이 정상적으로 되어야 합니다',  () => {
-    render(<App />);
-    expect(screen.getByRole('main')).toBeInTheDocument();
-  });
-}); 
